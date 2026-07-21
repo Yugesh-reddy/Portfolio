de1940f702ac3c796b2dd87eaccd612679f6316f
Yugesh-reddy <yugeshreddysappidi@gmail.com>
2026-07-21 13:54:25 -0500

    Adjust footer branding

---
+++ src/components/site-footer-brand.tsx
@@ -0,0 +1,9 @@
+import { FluidGradientText } from "@/registry/components/fluid-gradient-text"
+
+export function SiteFooterInteractiveLogotype() {
+  return (
+    <div className="screen-line-bottom text-foreground">
+      <FluidGradientText text="YUGESH" />
+    </div>
+  )
+}
