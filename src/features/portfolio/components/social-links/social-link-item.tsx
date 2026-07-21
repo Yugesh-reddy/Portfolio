b4ef51dd95cc9fb8bb1eae1a1ccfda10048c04d2
Yugesh-reddy <yugeshreddysappidi@gmail.com>
2026-07-21 14:50:54 -0500

    Update social links

---
+++ src/features/portfolio/components/social-links/social-link-item.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import { addQueryParams } from "@/utils/url"
+import { ArrowUpRightIcon } from "lucide-react"
+
+import { UTM_PARAMS } from "@/config/site"
+import { cn } from "@/lib/utils"
+import type { SocialLink } from "@/features/portfolio/types/social-links"
+
+export function SocialLinkItem({ icon, title, href }: SocialLink) {
+  return (
+    <div
+      className={cn(
+        "group/social relative flex cursor-pointer items-center gap-4 p-4 pr-2 transition-colors duration-200 ease-out hover:bg-accent-muted"
+      )}
+    >
+      <div className="relative size-8 shrink-0 transition-transform duration-200 ease-out group-hover/social:scale-105 [--image-radius:var(--radius-lg)]">
+        <Image
+          className="rounded-(--image-radius) select-none"
+          src={icon}
+          alt={title}
+          width={32}
+          height={32}
+          quality={100}
+          unoptimized
+        />
+        <div className="pointer-events-none absolute inset-0 rounded-(--image-radius) inset-ring-1 inset-ring-black/10 dark:inset-ring-white/15" />
+      </div>
+
+      <h3 className="flex-1 font-medium">
+        <a
+          href={addQueryParams(href, UTM_PARAMS)}
+          target="_blank"
+          rel="noopener"
+        >
+          <span className="absolute inset-0" aria-hidden />
+          {title}
+        </a>
+      </h3>
+
+      <ArrowUpRightIcon className="size-4 text-muted-foreground transition-[transform,color] duration-200 ease-out group-hover/social:-translate-y-0.5 group-hover/social:translate-x-0.5 group-hover/social:text-foreground" />
+    </div>
+  )
+}
