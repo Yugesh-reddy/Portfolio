f67d0925f5801aed3ca6d8e49b9bf7719042739e
Yugesh-reddy <yugeshreddysappidi@gmail.com>
2026-07-27 09:25:36 -0500

    Refine certifications section

---
+++ src/features/portfolio/data/certifications.ts
@@ -0,0 +1,22 @@
+import type { Certification } from "../types/certifications"
+
+export const CERTIFICATIONS: Certification[] = [
+  {
+    title: "AWS Certified AI Practitioner",
+    issuer: "Amazon Web Services",
+    issuerLogoURL: "/icons/aws.svg",
+    issueDate: "2026-07-18",
+    credentialID: "",
+    credentialURL:
+      "https://www.credly.com/badges/7ae55924-7cee-432a-b257-88ba20eea3d3/public_url",
+  },
+  {
+    title: "The Complete 2024 Web Development Bootcamp",
+    issuer: "Udemy",
+    issuerLogoURL: "/icons/udemy.svg",
+    issueDate: "2024-01-26",
+    credentialID: "UC-df915780-fc98-480c-8441-dd734da33727",
+    credentialURL:
+      "https://www.udemy.com/certificate/UC-df915780-fc98-480c-8441-dd734da33727/",
+  },
+]
