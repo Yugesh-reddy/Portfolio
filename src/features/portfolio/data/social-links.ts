317abc7eb6ac32c478f4a4519e29eb7f54849e92
Yugesh-reddy <yugeshreddysappidi@gmail.com>
2026-08-03 09:05:50 -0500

    Update social links

---
+++ src/features/portfolio/data/social-links.ts
@@ -0,0 +1,22 @@
+import type { SocialLink } from "../types/social-links"
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    icon: "/icons/github.svg",
+    title: "GitHub",
+    subtitle: "Yugesh-reddy",
+    href: "https://github.com/Yugesh-reddy",
+  },
+  {
+    icon: "/icons/linkedin.svg",
+    title: "LinkedIn",
+    subtitle: "yugesh-reddy-sappidi",
+    href: "https://www.linkedin.com/in/yugesh-reddy-sappidi",
+  },
+  {
+    icon: "/icons/x.svg",
+    title: "X",
+    subtitle: "Yugeshsapp",
+    href: "https://x.com/Yugeshsapp",
+  },
+]
